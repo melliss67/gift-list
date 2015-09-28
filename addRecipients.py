a6402457c6496e23a53bbc31d0257cf7d66877f1
mellis <mellis@westmarkproducts.com>
2015-09-28 14:58:29 -0700

    more databse setup

--- addRecipients.py
+++ addRecipients.py
@@ -15,4 +15,12 @@ d = date(1983, 1, 1)
 rec1 = Recipients(name="Heather Webster",birthday=d)
 session.add(rec1)
 
+d2 = date(1969, 9, 3)
+rec2 = Recipients(name="Rich Ellis",birthday=d2)
+session.add(rec2)
+
+d3 = date(1949, 12, 15)
+rec3 = Recipients(name="Jo Ellis",birthday=d3)
+session.add(rec3)
+
 session.commit()
